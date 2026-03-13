bdb99b2c939efc13dcc3c5f355870af19dfa1c34
yasua006 <156091445+yasua006@users.noreply.github.com>
2026-03-13 12:58:59 +0100

    Lag input
    Lagde input - det er som en system

--- main.py
+++ main.py
@@ -9,16 +9,29 @@ from email.mime.text import MIMEText
 from config import *
 
 
+def ask(current_totp: str) -> None:
+    result = input(
+        f"Please enter the authentication code.\nThe authentication code has been sent to email: {to}\n")
+
+    if result == current_totp:
+        print("Success!")
+        sys.exit(0)
+    else:
+        print("Invalid authentication code!")
+        sys.exit(0)
+
 def main() -> None:
     totp: TOTP = TOTP(random_base32())
+    emailed_code: str = totp.now()
 
-    msg: MIMEText = MIMEText(totp.now())
+    msg: MIMEText = MIMEText("Your code is: " + totp.now())
+    # print(msg)
 
     if sender_email == to:
         print("Receiver can't be sender!")
         sys.exit(0)
 
-    msg['Subject'] = "Kode test - 2FA"
+    msg['Subject'] = "2FA Verification"
     msg['From'] = sender_email
     msg['To'] = to
 
@@ -29,6 +42,8 @@ def main() -> None:
         to_addrs=to,
         msg=msg.as_string())
     s.quit()
+
+    ask(emailed_code)
 
 if __name__ == '__main__':
     main()
